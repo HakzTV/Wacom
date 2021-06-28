85beb55e6777d5599faaddfa5b0139e48ae48497
Telvin Varfley <telvinvarfley@gmail.com>
2021-06-28 11:50:06 +0000

    Form UI complete

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+function toggleForm(){
+    let container = document.querySelector('#container');
+    container.classList.toggle('active')
+}
+
+let swap = document.querySelectorAll('.swap');
+for(var i= 0; i<swap.length; i++){
+    swap[i].addEventListener('click' ,()=>{
+        toggleForm();
+    })
+}
